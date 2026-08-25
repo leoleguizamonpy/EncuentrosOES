6ba60b8b8ddbb3b5aaaf0483f8fb3f4466ba00d7
leoleguizamonpy <140459842+leoleguizamonpy@users.noreply.github.com>
2026-08-25 11:40:26 -0300

    refactor(ui): expose OES UI system

---
+++ apps/web/ui/index.ts
@@ -0,0 +1,5 @@
+export { DataList, DataRow } from './data-list';
+export { EntityDrawer, FormActions } from './entity-drawer';
+export { ListToolbar } from './list-toolbar';
+export { PageHeader } from './page-header';
+export { StatusBadge } from './status-badge';
