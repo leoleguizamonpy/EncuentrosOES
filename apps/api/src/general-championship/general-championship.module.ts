36001b169088692172fff82e7fea6ba4eb7c38d4
leoleguizamonpy <140459842+leoleguizamonpy@users.noreply.github.com>
2026-08-26 01:30:33 -0300

    feat(api): wire general championship options

--- apps/api/src/general-championship/general-championship.module.ts
+++ apps/api/src/general-championship/general-championship.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { GeneralChampionshipOptionsController } from './general-championship-options.controller.js';
+import { GeneralChampionshipOptionsService } from './general-championship-options.service.js';
 import { GeneralChampionshipController } from './general-championship.controller.js';
 import { GeneralChampionshipService } from './general-championship.service.js';
 
 @Module({
-  controllers: [GeneralChampionshipController],
-  providers: [GeneralChampionshipService],
+  controllers: [GeneralChampionshipController, GeneralChampionshipOptionsController],
+  providers: [GeneralChampionshipService, GeneralChampionshipOptionsService],
 })
 export class GeneralChampionshipModule {}
